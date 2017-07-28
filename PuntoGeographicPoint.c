b2a36fffb86725f4023f6d2e09fbe8fecba17a28
santiagocaro <s.caro11@uniandes.edu.co>
2017-07-27 19:42:22 -0500

    Add files via upload

--- PuntoGeographicPoint.c
+++ PuntoGeographicPoint.c
@@ -7,8 +7,9 @@
 
 int ran_x(int n_x);
 int ran_y(int n_y);
-float R(int n_y, int n_x, int *matriz);
-float compara(int n_y, int n_x, int *matriz);
+float R(int n_y, int n_x, int *matriz,int ran_xi,int ran_yi,float ran_xf,float ran_yf);
+int indice(int i, int j, int n_x, int n_y);
+void texto(int ran_xi, int ran_yi, float r_old);
 
 int main(){
 	FILE*file;
@@ -38,26 +39,63 @@ int main(){
 	}
 	i+=1;
 	}
-	//matriz[0][1]=23;
-	//printf("%d\n",matriz[0][1]);
+	
 	
 	srand (time(NULL)); 
+	srand48(1);
 	
 	int n_x = 500;
 	int n_y = 744;
 	
-	//ran_x(n_x);
-	//ran_y(n_y);
+	int iteraciones =10000;
 	
-	compara(n_y,n_x,matriz);
+	int ran_xi = ran_x(n_x);
+	int ran_yi = ran_y(n_y);
+	float ran_xf = ran_xi;
+	float ran_yf = ran_yi;
+	float r_old;
+	float r_new;
+
+	float alfa;
+	float beta;
+	beta = drand48();
+	char s[200] = "Las coordenadas del punto mas alejado son:";
+	char r[20] = "Radio:";
+	//float n = ((1-2*drand48())*10);
+	//printf("%f\n", n);
 	
-		
+	r_old=R(n_y,n_x,matriz,ran_xi,ran_yi,ran_xf,ran_yf);
+	//printf("%f\n", r_old);
+
+	for(i=0;i<iteraciones;i++){
+		int ran_xin = (int)ran_xi + ((1-2*drand48())*100);
+		int ran_yin = (int)ran_yi + ((1-2*drand48())*100);
+		float ran_xfn = ran_xin;
+		float ran_yfn = ran_yin;
+		r_new=R(n_y,n_x,matriz,ran_xin,ran_yin,ran_xfn,ran_yfn);
+		alfa = exp((r_new-r_old));
 	
+	if(alfa>1){
+		ran_xi = ran_xin;
+		ran_yi = ran_yin;
+		r_old=r_new;
+	}
+	else{
+		if(alfa > beta){
+			ran_xi = ran_xin;
+			ran_yi = ran_yin;
+			r_old=r_new;
+		}
+	}
+			
+	}
+	printf("%s %d %d \n", s, ran_xi, ran_yi);	
+	printf("%s %f\n", r, r_old);
+	texto(ran_xi, ran_yi, r_old);
 	return 0;
 }            
 
 int ran_x(int n_x){
-	
 	int rand_num = rand() % (n_x+1);
 	return rand_num;
 	//printf("%d\n",rand_num);
@@ -68,49 +106,47 @@ int ran_y(int n_y){
 	//printf("%d\n",rand_num);
 }
 
-float R(int n_y, int n_x, int *matriz){
-	char s[200] = "Las coordenadas del punto mas alejado son:";
-	int ran_xi = ran_x(n_x);
-	int ran_yi = ran_y(n_y);
-	float ran_xf = ran_xi;
-	float ran_yf = ran_yi;
-	//printf("%f\n",ran_yf+1); 
-	//printf("%d\n",ran_yi);
+int indice(int i, int j, int n_x, int n_y){
+	if(i > n_x){
+	i = i % n_x; 
+	}
+	if(j > n_y){
+	j = j % n_y; 
+	}
+	if(i < 0){
+		i = n_x + i;
+	}
+	if(j < 0){
+		j = n_y + j;
+	}
+	return n_y * i + j;
+}
+
+float R(int n_y, int n_x, int *matriz,int ran_xi,int ran_yi,float ran_xf,float ran_yf){
 	
-	int r;
-	float rf;
 	int i;
 	int j;
+	int r;
 	float distancia;
 	
-	if (matriz[n_y*ran_xi+ran_yi] != 1){
+	if (matriz[indice(ran_xi,ran_yi,n_x,n_y)] != 1){
 		for(r=1;r<n_x/2;r++){
 			for(i=-r;i<r;i++){
-				for(j=-r;i<r;i++){
+				for(j=-r;j<r;j++){
 					distancia = pow((pow((ran_xf+i)-ran_xf,2)+ pow((ran_yf+j)-ran_yf,2)),0.5);
-					if(matriz[n_y*(ran_xi+i)+(ran_yi+j)] == 1 &&  distancia <= r){
+					if(matriz[indice(ran_xi+i,ran_yi+j,n_x,n_y)] == 1 &&  distancia <= r){
 						return distancia;
-						break;	
 					}
 				}
 			}
 		}
-	printf("%f\n", distancia);
-	printf("%s %d %d \n", s, ran_xi, ran_yi);
 	}
 }
 
-float compara(int n_y, int n_x, int *matriz){
-	float r_max = R(n_y,n_x,matriz);
-	int n;
-	for(n=0;n<100;n++){
-		if(R(n_y,n_x,matriz)>r_max){
-			r_max=R(n_y,n_x,matriz);
-			return r_max;
-		}
-	}
-	
+void texto(int ran_xi, int ran_yi, float r_old){
+	FILE *file;
+	file=fopen("salida.txt","w");
+	fprintf(file, "%d %d %f \n", ran_xi, ran_yi, r_old );
 }
-
 
 
